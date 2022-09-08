edc3b0483f308dafdd97c24fcf868b246011166c
Ben Robuck <brobuck87@gmail.com>
2022-09-08 17:43:21 -0400

    Add object for different brew methods. Needs fix for js to add to page on user click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 const targetEl = document.getElementById("drawer-navigation");
+const grindEl = $("#grind-size");
+const espressoEl = $("#espresso");
+const coldBrewEl = $("#cold-brew");
+const dripEl = $("#drip");
+const aeroEl = $("#aero-press");
+const frenchEl = $("#french-press");
+const pourEl = $("#pour-over");
 
 // options with default values
 const options = {
@@ -8,13 +15,59 @@ const options = {
   edge: false,
   edgeOffset: "",
   backdropClasses:
-    "bg-chocolate-800 dark:bg-chocolate-800 bg-opacity-50 dark:bg-opacity-50 fixed inset-0 z-30",
+    "bg-ltcrm dark:bg-chocolate-800 bg-opacity-50 dark:bg-opacity-50 fixed inset-0 z-30",
 };
 
 const drawer = new Drawer(targetEl, options);
 
-window.addEventListener("load", (event) => {
-  drawer.hide();
-  console.log("page is fully loaded");
+const brewList = {
+  "cold-brew": {
+    "grind-size": "Extra Coarse",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  },
+  "french-press": {
+    "grind-size": "Coarse",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  },
+  "drip": {
+    "grind-size": "Medium",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  },
+  "pour-over": {
+    "grind-size": "Medium-Fine",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  },
+  "espresso": {
+    "grind-size": "Fine Grind",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  },
+  "aero-press": {
+    "grind-size": "Fine Grind",
+    "bean-weight": "",
+    "water-temperature": "",
+    "ratio": 17.42
+  }
+}
+
+[espressoEl, coldBrewEl, dripEl, aeroEl, frenchEl, pourEl].forEach((element) => {
+  element.click((e) => {
+    grindEl.text(brewList[element.attr('id')].grind-size)
+    // console.log("I'm amazing")
+    // console.log(brewList[element.attr('id')], element, grindEl)
+    // console.log(grindEl)
+  });
 });
-// document.onload(drawer.hide());
+
+// Listen to click for each element
+// Based on click select the right grind size
+// Put grind size onto the page
